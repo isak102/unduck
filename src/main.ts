746ea42c84ad3eb71097c57ab08377ab6b62af3b
Isak <isak.drevstad10@gmail.com>
2025-03-22 15:14:03 +0100

    Change urls

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { bangs } from "./bang";
 import "./global.css";
 
+// TODO: Mount a svelte component instead of this, but still needs to be PWA
+
 function noSearchDefaultPageRender() {
   const app = document.querySelector<HTMLDivElement>("#app")!;
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Improved Und*ck</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
         <div class="url-container"> 
           <input 
@@ -21,11 +23,7 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/isak102/unduck" target="_blank">github</a>
       </footer>
     </div>
   `;
